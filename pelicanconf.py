b78787de230e1ccb926ef2c0c0d50960783136a1
40423245 <40423245@gm.nfu.edu.tw>
2016-10-28 10:57:14 +0800

    copy 2016fallcadpa

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = '40423245'
-SITENAME = '40423245 課程網誌 '
+AUTHOR = 'KMOL'
+SITENAME = '2016Fall 課程網誌 (虎尾科大MDE)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
